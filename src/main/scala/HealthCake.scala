2046da0663f3ef65f5f8067eada4809a54293584
Alois Cochard <alois.cochard@gmail.com>
2013-10-27 21:05:32 +0000

    Add check for unsafe operation on Iterable.

--- src/main/scala/HealthCake.scala
+++ src/main/scala/HealthCake.scala
@@ -19,11 +19,22 @@ trait HealthCake {
   object Doctor {
     class StdLib extends Doctor {
       def name = "std-lib"
+
+      val unsafeOnEmptyIterable = Seq("head", "last", "reduce", "reduceLeft", "reduceRight")
+
       val diagnostic: PartialFunction[PhaseId, CompilationUnit => Seq[(Position, Message)]] = {
         case "parser" => _.body.collect {
           case tree@Select(_, name) if name.toString == "asInstanceOf" => 
             tree.pos -> "There should be a better way than using `asInstanceOf`, what do you think?"
         }
+        case "typer" => unit => 
+          unit.body.collect {
+            case tree@Select(value, name) if unsafeOnEmptyIterable.contains(name.toString) && value.tpe <:< typeOf[Iterable[Any]] => 
+              tree.pos -> (
+                s"Are we sure the `${value.tpe.typeSymbol.name}` will never be empty?\n" +
+                s"Because calling `$name` might throw an exception in this case."
+              )
+          }
       }
     }
   }
