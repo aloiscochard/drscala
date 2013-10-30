6df3cc85cad12ae23fbbc14e29ef29c046b696cc
Alois Cochard <alois.cochard@gmail.com>
2013-10-30 18:19:46 +0000

    Fix `Nothing` type inference checking.
    Using @puffnfresh technic used in WartRemover.

--- src/main/scala/HealthCake.scala
+++ src/main/scala/HealthCake.scala
@@ -22,6 +22,11 @@ trait HealthCake {
 
       val unsafeOnEmptyIterable = Seq("head", "last", "reduce", "reduceLeft", "reduceRight")
 
+      def isNothingInferred(tree: Tree) = PartialFunction.cond(tree) {
+        case ValDef(_, _, tpt, _) if tpt.exists(_.tpe =:= typeOf[Nothing]) => true
+        case DefDef(_, _, _, _, tpt, _) if tpt.exists(_.tpe =:= typeOf[Nothing]) => true
+      }
+
       val diagnostic: PartialFunction[PhaseId, CompilationUnit => Seq[(Position, Message)]] = {
         case "parser" => _.body.collect {
           case tree@Ident(name) if name.toString == "$qmark$qmark$qmark" => 
@@ -34,7 +39,7 @@ trait HealthCake {
             tree.pos -> "There should be a better way than using `asInstanceOf`, what do you think?"
         }
         case "typer" => _.body.collect {
-          case tree@TypeApply(_, xs) if xs.forall(_.tpe =:= typeOf[Nothing]) =>
+          case tree if isNothingInferred(tree) =>
             tree.pos -> "I feel a disturbance in the force, the type `Nothing` might have been inferred."
 
           case tree@Select(value, name) if unsafeOnEmptyIterable.contains(name.toString) && value.tpe <:< typeOf[Iterable[Any]] => 
